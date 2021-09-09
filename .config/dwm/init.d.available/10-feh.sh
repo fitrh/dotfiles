642a3c0a0e96c8b70eae66dd847123acb54425c3
fitrh <fitrahm17h@student.unhas.ac.id>
2021-09-09 22:36:56 +0800

    config/dwm: Remove unused init.d scripts
    The `10-feh.sh` and `10-xwallpaper.sh` is replaced by `10-setbg.sh`,
    which is a wrapper script to set the desktop background using `feh` or
    `xwallpaper`.

--- .config/dwm/init.d.available/10-feh.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-[[ -f ~/.fehbg ]] && [[ -x ~/.fehbg ]] && ~/.fehbg &
--- .config/dwm/init.d.available/10-xwallpaper.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-WALLPAPER_DIR="$HOME/pictures/wallpapers"
-WALLPAPER="$WALLPAPER_DIR/Earth & The Satelite - Tokyo Night Darker.png"
-xwallpaper --clear --daemon --zoom "$WALLPAPER"
