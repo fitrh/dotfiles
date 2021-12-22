7698afa73300ef2f88bfb349ef1f07dfbbf0654d
fitrh <fitrahm17h@student.unhas.ac.id>
2021-12-22 16:19:44 +0800

    config/dwm/init: Set screen backlight and temperature base on hour

--- .config/dwm/init.d.available/12-set_backlight_gamma.sh
+++ .config/dwm/init.d.available/12-set_backlight_gamma.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
 
-set_backlight_gamma() {
-    local BACKLIGHT GAMMA
+set_backlight_gamma() (
+    local BACKLIGHT GAMMA HOUR
     local STATE="${XDG_STATE_HOME:-$HOME/.local/state}/clightctl"
-    BACKLIGHT="$(<"${STATE}/backlight")"
-    GAMMA="$(<"${STATE}/gamma")"
+    HOUR=$(date +"%-H")
+    BACKLIGHT=$(<"$STATE/backlight")
+    BACKLIGHT=$(bc <<<"scale=0; $BACKLIGHT * 100 / 1")
+    GAMMA=$(<"$STATE/gamma")
+
+    if [[ "$HOUR" -gt 20 || "$HOUR" -lt 8 ]]; then
+        [[ "$GAMMA" -ge 6400 ]] && GAMMA=4500
+        [[ "$BACKLIGHT" -ge 40 ]] && BACKLIGHT=20
+    else
+        [[ "$GAMMA" -lt 6400 ]] && GAMMA=6400
+        [[ "$BACKLIGHT" -lt 50 ]] && BACKLIGHT=50
+    fi
 
     clightctl backlight set "$BACKLIGHT"
     clightctl gamma set "$GAMMA"
-}
+)
 
 set_backlight_gamma
