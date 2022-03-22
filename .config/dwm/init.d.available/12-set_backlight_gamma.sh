b31f133cc099fed604afdef7e2623443801348e7
fitrh <fitrahm17h@student.unhas.ac.id>
2022-03-22 18:25:30 +0800

    config/dwm/init.d/12-set_backlight_gamma: Remove the use of `bc`

--- .config/dwm/init.d.available/12-set_backlight_gamma.sh
+++ .config/dwm/init.d.available/12-set_backlight_gamma.sh
@@ -5,9 +5,9 @@ set_backlight_gamma() (
     local STATE="${XDG_STATE_HOME:-$HOME/.local/state}"
     local NIGHT_MODE="$STATE/clightctl/is_night_mode"
     local CLIGHT_STATE="$STATE/clightctl"
+
     HOUR=$(date +"%-H")
     BACKLIGHT=$(<"$CLIGHT_STATE/backlight")
-    BACKLIGHT=$(bc <<<"scale=0; $BACKLIGHT * 100 / 1")
     GAMMA=$(<"$CLIGHT_STATE/gamma")
 
     if [[ "$HOUR" -ge 20 || "$HOUR" -lt 8 ]]; then
