3e9d7f37fc37827b72117b44f392f8d5ed551aaa
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2022-06-14 14:01:33 +0800

    local/src/c/colorescape: Update format, add leading zero

--- .local/src/c/colorescape.c
+++ .local/src/c/colorescape.c
@@ -4,7 +4,7 @@ int main(void) {
         for (int i = 0; i < 11; i++) {
                 for (int j = 0; j < 10; j++) {
                         int n = 10 * i + j;
-                        printf("\033[%dm %3d\033[m", n, n);
+                        printf("\033[%dm %03d \033[m ", n, n);
                 }
                 printf("\n");
         }
